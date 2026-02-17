c7689d0beca61f576efa45f0bbca91dd4b6e6fa7
unknown <margaretjanet619@gmail.com>
2026-02-17 23:53:06 +0300

    css

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import PharmaLogin from "./PharmaLogin";
 import HospitalLogin from "./HospitalLogin";
+import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/pharma-login" element={<PharmaLogin />} />
-        <Route path="/hospital-login" element={<HospitalLogin />} />
-      </Routes>
-    </BrowserRouter>
+   
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/pharma-login" element={<PharmaLogin />} />
+          <Route path="/hospital-login" element={<HospitalLogin />} />
+        </Routes>
+      </BrowserRouter>
+  
   );
+
+  
 }
 
 export default App;
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useState} from "react";
 import { Link } from "react-router-dom";
+import "./App.css";
 function HomePage() {
+    const [showOptions, setShowOptions] = useState(false);
+
     return (
-        <div style={{ textAlign: "center", marginTop: "50px"}}>
-            <hi>Drug Distribution System</hi>
-            <h2>Welcome! Please Sign In:</h2>
+        <div className="homepage">
+             <div className="navbar">
+        <div className="nav-left">
+          <h1>M.J PHARMACEUTICAL</h1>
+        </div>
+        <div className="nav-right">
+          <button className="login-btn" onClick={() => setShowOptions(!showOptions)}>
+            Login
+          </button>
+          {showOptions && (
+            <div className="dropdown">
+              <Link to="/pharma-login">
+                <button className="option-btn">Pharmaceutical</button>
+              </Link>
+              <Link to="/hospital-login">
+                <button className="option-btn">Hospital</button>
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+
+            
+            <h2>Mission</h2>
+            <p className="mission">
+                To ensure safe, efficient, and reliable distribution of pharmaceutical products, bridging the gap between manufacturers and hospitals to improve patient care and community health.
+            </p>
+            <h2>Vision</h2>
+            <p className="vision">
+                To be the leading pharmaceutical distributor recognized for innovation, trust, and excellence in delivering life saving medicines across healthcare institutiins. 
+            </p>
             <div style={{ marginTop: "20px"}}>
                 <Link to="/pharma-login">
                 <button style={{ margin: "10px", padding: "10px 20px"}}>
-                    Sign in as M.J Pharmaceutical
+                     Pharmaceutical
                 </button>
                 
                 </Link>
                 <Link to="/hospital-login">
                 <button style={{ margin: "10px", paddin: "10px 20px"}}>
-                    Sign in as Hospital
+                     Hospital
                 </button>
                 </Link>
             </div>
